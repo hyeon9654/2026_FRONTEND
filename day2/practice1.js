cbf2b760dcdb8863ea49d52b0b73940a9ca3c5a2
hyeon9654 <pjoo6067@gmail.com>
2026-07-07 18:00:23 +0900

    day2 6h

---
+++ day2/practice1.js
@@ -0,0 +1,37 @@
+let 주문접수 = "[주문 접수]"
+let 이름상자1 = "이름"
+let 이름상자2 = "메뉴"
+let 이름상자3 = "수량"
+
+let 결과1 = prompt("이름")  
+let 결과2 = prompt("원하는 음료") 
+let 결과3 = prompt("주문 수량")  
+
+
+console.log( 주문접수 )
+console.log( 이름상자1 )
+console.log( 결과1 )
+console.log( 이름상자2 )
+console.log( 결과2 )
+console.log( 이름상자3 )
+console.log( 결과3 )
+
+let 운동기록 = "[운동 기록]"
+let 이름상자4 = "이름"
+let 이름상자5 = "운동종류"
+let 이름상자6 = "운동시간"
+let 이름상자7 = "칼로리"
+
+let 결과4 = prompt("이름")  
+let 결과5 = prompt("운동종류") 
+let 결과6 = prompt("운동시간(분)")  
+let 결과7 =  결과6 * 7
+console.log( 운동기록 )
+console.log( 이름상자4 )
+console.log( 결과4 )
+console.log( 이름상자5 )
+console.log( 결과5 )
+console.log( 이름상자6 )
+console.log( 결과6 )
+console.log( 이름상자7 )
+console.log( 결과7 )
